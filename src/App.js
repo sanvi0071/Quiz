e530515df22ef05e83a5352ba4596f520418a73d
Sachin Kumar <mail.sachin161@gmail.com>
2023-04-08 01:46:40 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-
-import './App.css';
-import Menu from './component/menu';
-import EndScreen from './component/EndScreen';
-import Quiz from './component/quiz';
-import { useState } from 'react';
-import {GameStateContext} from "./helpers/context";
+import "./App.css";
+import Menu from "./component/menu";
+import EndScreen from "./component/EndScreen";
+import Quiz from "./component/quiz";
+import { useState } from "react";
+import { GameStateContext } from "./helpers/context";
 
 function App() {
-  const [gameState, setGameState]=useState("menu");
-  const [userName, setUserName]=useState("");
-  const [score, setScore]=useState(0);
+  const [gameState, setGameState] = useState("menu");
+  const [userName, setUserName] = useState("");
+  const [score, setScore] = useState(0);
   return (
     <div className="App">
-     <h1>Quiz App</h1>
-     <GameStateContext.Provider value={{gameState,
-       setGameState , 
-       userName, 
-       setUserName,
-       score,
-       setScore,
-       }}>
-     {gameState==="menu" &&  <Menu/>}
-     {gameState==="playing" && <Quiz/>}
-     {gameState==="finished" && <EndScreen/>}
-    </GameStateContext.Provider>
-  </div>
+      <h1>Quiz App</h1>
+      <GameStateContext.Provider
+        value={{
+          gameState,
+          setGameState,
+          userName,
+          setUserName,
+          score,
+          setScore,
+        }}
+      >
+        {gameState === "menu" && <Menu />}
+        {gameState === "playing" && <Quiz />}
+        {gameState === "finished" && <EndScreen />}
+      </GameStateContext.Provider>
+    </div>
   );
-
 }
 
 export default App;
